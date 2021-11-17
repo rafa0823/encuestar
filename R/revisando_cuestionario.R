0ac6b61488c3da370ed68427c524571fa6ea21af
datos-morant <datosmorant@gmail.com>
2021-11-17 17:31:19 -0600

    sube script para revisar cuestionario con tipo de pregunta

--- R/revisando_cuestionario.R
+++ R/revisando_cuestionario.R
@@ -1,24 +1,25 @@
-
-
-cuestionario <- officer::read_docx("~/Downloads/Encuesta_Plantilla_16Nov.docx")
-
 revisando_cuestionario <- function(base){
-  bd <- officer::docx_summary(base) %>%
-  filter(!is.na(style_name),style_name %in% c("Morant_Bloque","Morant_Pregunta",
-                                              "Morant_respuetas")) %>%
-  select(-c(level:row_span)) %>%
-  mutate(bloque=ifelse(style_name=="Morant_Bloque", text, NA)) %>%
-  fill(bloque,.direction = c("down")) %>%
-  filter(!style_name=="Morant_Bloque") %>%
-  mutate(pregunta=ifelse(style_name=="Morant_Pregunta", text, NA)) %>%
-  fill(pregunta,.direction = c("down")) %>%
-  filter(!style_name=="Morant_Pregunta") %>%
-  group_by(bloque, pregunta) %>%
-  summarise(respuestas=list(text)) %>%
-  ungroup() %>%
-  mutate(llaves=stringr::str_extract(pregunta, "\\{.+\\}"))
+  bd <- officer::docx_summary(cuestionario) %>%
+    filter(!is.na(style_name),style_name %in% c("Morant_Bloque","Morant_Pregunta",
+                                                "Morant_respuestas_abiertas",
+                                                "Morant_respuestas_numericas",
+                                                "Morant_respuetas_multiples")) %>%
+    select(-c(level:row_span)) %>%
+    mutate(bloque=ifelse(style_name=="Morant_Bloque", text, NA)) %>%
+    fill(bloque,.direction = c("down")) %>%
+    filter(!style_name=="Morant_Bloque") %>%
+    mutate(pregunta=ifelse(style_name=="Morant_Pregunta", text, NA)) %>%
+    fill(pregunta,.direction = c("down")) %>%
+    filter(!style_name=="Morant_Pregunta") %>%
+    separate(style_name, c("a", "b", "c"), sep = "_") %>%
+    rename("tipo_pregunta"="c") %>%
+    group_by(bloque, pregunta, tipo_pregunta) %>%
+    summarise(respuestas=list(text)) %>%
+    ungroup() %>%
+    mutate(llaves=stringr::str_extract(pregunta, "(?<=\\{).+?(?=\\})"),
+           llaves=stringr::str_squish(llaves),
+           pregunta=stringr::str_remove(pregunta, "\\{.+\\}"),
+           pregunta=stringr::str_squish(pregunta))
 
   return(bd)
 }
-
-revisando_cuestionario(cuestionario)
