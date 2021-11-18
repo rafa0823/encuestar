4b91d28062546196e582a9dd3571d8c19baa6390
gorantesj <gorantesj@hotmail.com>
2021-11-18 17:01:04 -0600

    Arreglar el script de Héctor

--- R/revisando_cuestionario.R
+++ R/revisando_cuestionario.R
@@ -1,20 +1,26 @@
 
-revisando_cuestionario <- function(base){
-  bd <- officer::docx_summary(base) %>%
-  filter(!is.na(style_name),style_name %in% c("Morant_Bloque","Morant_Pregunta",
-                                              "Morant_respuetas")) %>%
-  select(-c(level:row_span)) %>%
-  mutate(bloque=ifelse(style_name=="Morant_Bloque", text, NA)) %>%
-  fill(bloque,.direction = c("down")) %>%
-  filter(!style_name=="Morant_Bloque") %>%
-  mutate(pregunta=ifelse(style_name=="Morant_Pregunta", text, NA)) %>%
-  fill(pregunta,.direction = c("down")) %>%
-  filter(!style_name=="Morant_Pregunta") %>%
-  group_by(bloque, pregunta) %>%
-  summarise(respuestas=list(text)) %>%
-  ungroup() %>%
-  mutate(llaves=stringr::str_extract(pregunta, "\\{.+\\}"))
+revisando_cuestionario <- function(doc){
+  bd <- officer::docx_summary(doc) %>%
+    as_tibble() %>%
+    filter(!is.na(style_name),style_name %in% c("Morant_Bloque","Morant_Pregunta",
+                                                "Morant_respuestas_abiertas",
+                                                "Morant_respuestas_numericas",
+                                                "Morant_respuetas_multiples")) %>%
+    select(-c(level:row_span)) %>%
+    mutate(bloque=ifelse(style_name=="Morant_Bloque", text, NA)) %>%
+    fill(bloque,.direction = c("down")) %>%
+    filter(style_name!="Morant_Bloque") %>%
+    mutate(pregunta=ifelse(style_name=="Morant_Pregunta", text, NA)) %>%
+    fill(pregunta,.direction = c("down")) %>%
+    filter(!style_name=="Morant_Pregunta") %>%
+    separate(style_name, c("a", "b", "tipo_pregunta"), sep = "_") %>%
+    group_by(bloque, pregunta, tipo_pregunta) %>%
+    summarise(respuestas=list(text)) %>%
+    ungroup() %>%
+    mutate(llaves=stringr::str_extract(pregunta, "(?<=\\{).+?(?=\\})"),
+           llaves=stringr::str_squish(llaves),
+           pregunta=stringr::str_remove(pregunta, "\\{.+\\}"),
+           pregunta=stringr::str_squish(pregunta))
 
   return(bd)
 }
-
