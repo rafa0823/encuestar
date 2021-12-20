6adbae9fedb457b171c7f493c95f4d20e8bae72f
gorantesj <gorantesj@hotmail.com>
2021-12-20 10:54:19 -0600

    Faltaban algunas cosas

--- R/procesar_surveytogo.R
+++ R/procesar_surveytogo.R
@@ -1,15 +1,41 @@
 
 
 analizar_frecuencias <- function(encuesta, pregunta){
+  # browser()
+  # encuesta$diccionario %>%
+  #   filter(pregunta==rlang::expr_text(ensym(pregunta)))
   estimacion <-survey::svymean(enquo(pregunta),
-               design = encuesta$diseño) %>%
+                               design = encuesta$diseño) %>%
     tibble::as_tibble(rownames = "respuesta") %>%
     rename(media=mean, ee=SE) %>%
     mutate(respuesta = stringr::str_replace(
       pattern = rlang::expr_text(ensym(pregunta)),
       replacement = "",
       string = respuesta),
-           respuesta=forcats::fct_reorder(.f = respuesta,
-                                          .x = media,
-                                          .fun = max))
+      respuesta=forcats::fct_reorder(.f = respuesta,
+                                     .x = media,
+                                     .fun = max))
+
+}
+
+analizar_frecuencias_aspectos <- function(encuesta, pregunta, aspectos){
+  llaves <- glue::glue("{rlang::expr_text(ensym(pregunta))}_{aspectos}")
+
+  estimaciones <- map_df(llaves,
+                      ~{
+                        survey::svymean(survey::make.formula(.x),
+                                        design = encuesta$diseño) %>%
+                          tibble::as_tibble(rownames = "respuesta") %>%
+                          rename(media=mean, ee=SE) %>%
+                          mutate(
+                            aspecto=glue::glue("temporal{.x}"),
+                            respuesta = stringr::str_replace(
+                            pattern = .x,
+                            replacement = "",
+                            string = respuesta),
+                            respuesta=forcats::fct_reorder(.f = respuesta,
+                                                           .x = media,
+                                                           .fun = max))
+                      })
+
 }
