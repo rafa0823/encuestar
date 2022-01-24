c489c3ce9fe903b2c7e72fcd9b38b2a6de16b154
Déborah <deborahandrea.m@hotmail.com>
2022-01-24 10:06:01 -0600

    prueba 24

--- R/graficar.R
+++ R/graficar.R
@@ -119,6 +119,7 @@ graficar_frecuencia_opuestos <- function(bd,titulo, grupo1, grupo2,
     theme(legend.position = "none")
 }
 
+
 graficar_aspectos_frecuencias <- function(bd,   titulo= NULL,
                                        fill=NULL,
                                        nota = "",
