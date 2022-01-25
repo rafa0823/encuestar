364d388703e1616372ed58f11471a3b83069463f
Katherine Hernandez <khernenadez@iMac-de-Katherine.local>
2022-01-25 17:09:09 -0600

    Ajustes y cambios a la gráfica de aspectos

--- R/graficar.R
+++ R/graficar.R
@@ -20,10 +20,10 @@ graficar_barras_frecuencia <- function(bd,
   g <-  bd %>% ggplot(aes(x = forcats::fct_reorder(stringr::str_wrap(respuesta,40),
                                                    media),
                           y  = media,
-                          fill=color))+
+                          fill=respuesta))+
     ggchicklet::geom_chicklet(radius = grid::unit(3, "pt"),
                               alpha= .8,
-                              width =.45, fill=color)+
+                              width =.45)+
     labs(title = titulo,
          x = NULL,
          y = NULL,
@@ -133,14 +133,14 @@ graficar_aspectos_frecuencias <- function(bd,   titulo= NULL,
                                                     "Aprueba poco" = "#126782",
                                                     "Aprueba mucho" = "#023047",
                                                     "Ns/Nc" = "gray"),
-                                       orden = c("Desaprueba mucho",
-                                                 "Desaprueba poco",
+                                       orden = c("Desaprueba poco","Desaprueba mucho",
+
                                                  "Aprueba poco",
-                                                 "Aprueba mucho")){
+                                                 "Aprueba mucho"),
+                                       familia = "Poppins"){
 
   transparencia <- .8
   ancho_barras <- .45
-  familia <- "Poppins"
   color_etiqueta <- "#3B3838"
 
 
@@ -149,29 +149,42 @@ graficar_aspectos_frecuencias <- function(bd,   titulo= NULL,
     mutate(etiqueta = media,
            media = case_when(respuesta %in%grupo_negativo~media*-1,
                              respuesta %in% grupo_positivo~media,
-                             respuesta == ns_nc~media+1),
+                             respuesta == ns_nc~media+1.01),
            media2 = case_when(respuesta == ns_nc~0, T~media)) %>%
-    group_by(aspecto) %>%  mutate(saldo=sum(media2))
+    group_by(tema) %>%  mutate(saldo=sum(media2),
+                               color_saldo = case_when(saldo>0~"#fb8500", saldo<0~"#126782",
+                                                       saldo ==0 ~"gray")) %>%  ungroup()
 
 
   aux %>%  filter(respuesta != ns_nc) %>%
-    ggplot(aes(x  =forcats::fct_reorder(aspecto, saldo), fill = respuesta, y =media,
+    ggplot(aes(x  =forcats::fct_reorder(tema, saldo), fill = respuesta, y =media,
                group =factor(respuesta, levels = orden) )) +
     geom_chicklet(stat = "identity", width = ancho_barras, alpha = transparencia)+
-    geom_text(aes(label = scales::percent(media,accuracy = 1)), family = familia,
-              position = position_stack(.5,reverse = T), vjust = .5,
-              color = color_etiqueta) +
     scale_fill_manual(values = colores)+
     scale_y_continuous(labels=scales::percent_format(accuracy = 1))+
     coord_flip()+
     labs(title = titulo, x = NULL, y = "", fill = NULL)+
     geom_segment(data=aux %>%  filter(respuesta== ns_nc),
-                 mapping=aes(x = aspecto, y = 1,
-                             xend=aspecto, yend= media), size = 10,
+                 mapping=aes(x = tema, y = 1,
+                             xend=tema, yend= media), size = 10,
                  color = "gray")+
+    # geom_segment(aes(y =0, yend = 1), size = 1)+
     geom_text(data=aux %>%  filter(respuesta== ns_nc),
               aes(label = scales::percent(etiqueta,accuracy = 1)),
-              hjust = 'outside', nudge_x = 0, nudge_y = .025)
+              hjust = 'outside', nudge_x = 0, nudge_y = .025, family = familia)+
+    geom_text(data =  aux %>% filter(respuesta == ns_nc),
+              aes(color = color_saldo, y=saldo,
+                  label =paste("Saldo: ",scales::percent(saldo, accuracy = 1))),
+              family = familia, size = 3.5, hjust = .5,nudge_x = .3,  nudge_y =-0.08, show.legend = F)+
+    geom_point(data =  aux %>% filter(respuesta == ns_nc), shape = 18, size = 3,
+               aes(x = tema, y=saldo, color = color_saldo), show.legend = F, position = position_nudge(x =.3))+
+    geom_text(aes(label = scales::percent(media,accuracy = 1)), family = familia,
+              position = position_stack(.7,reverse = T),
+              color = color_etiqueta) +
+    geom_hline(yintercept = 0, color = "#FFFFFF", size= .6)+
+    geom_hline(yintercept = 0, color = "gray", size= .6)+
+    geom_hline(yintercept = 1, color = "#FFFFFF", size = 1.2)+
+    geom_hline(yintercept = 1, color = "#323232", linetype = "dotted", size = .7)
 
 }
 
@@ -199,13 +212,13 @@ sustituir <- function(bd, patron, reemplazo = ""){
 
 graficar_barras_numerica<- function(bd){
 bd %>%
-  ggplot(aes(y = media, x = reorder(str_wrap(aspecto,40),media))) +
+  ggplot(aes(y = media, x = reorder(str_wrap(tema,40),media))) +
   geom_chicklet(radius = grid::unit(3, "pt"),
                 alpha= .95,fill = "#850D2D",
                 width =.45)+ coord_flip()+
   labs(title = NULL,
        x = NULL,
-       y = NULL)+
+       y = "Promedio")+
   ggfittext::geom_bar_text(aes(label= round(media,digits = 1)),
                            contrast = T)+
   theme(panel.grid.major.x =element_line(colour = "#C5C5C5",
