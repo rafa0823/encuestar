e3899c489081ee79c3ec297dc81348daf5b9fa99
Déborah Martínez <deborahandrea.m@hotmail.com>
2022-03-17 12:03:26 -0600

    detalles en barras palabras

--- R/graficar.R
+++ R/graficar.R
@@ -294,14 +294,15 @@ graficar_barras_palabras <- function(bd, pregunta, nota, tit, n = 10){
     ggplot(aes(x = forcats::fct_reorder(palabras, n), y = n))+
     ggchicklet::geom_chicklet(radius = grid::unit(3, "pt"),
                               alpha= .8,
-                              width =.45)+
+                              width =.45,
+                              fill = "#871938")+
+    theme_minimal()+
     labs(title = tit,
          x = NULL,
          y = NULL,
          caption = nota)+
     coord_flip()+
-    scale_y_continuous(labels=scales::percent_format(accuracy = 1))+
-    ggfittext::geom_bar_text(aes(label=scales::percent(n, accuracy = 1)),contrast = T)
+    ggfittext::geom_bar_text(aes(label=n),contrast = T)
 }
 
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("colores"))
