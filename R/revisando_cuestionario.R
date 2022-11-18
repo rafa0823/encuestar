cccd3c6f36d66b3ddf3bacf109104a2bde797a20
EAMI91 <emiliomorones@gmail.com>
2022-11-18 16:11:15 -0600

    multitabla

--- R/revisando_cuestionario.R
+++ R/revisando_cuestionario.R
@@ -50,6 +50,13 @@ diccionario_cuestionario <- function(doc, patron){
     select(tipo_pregunta, text, pregunta, bloque) %>%
     pivot_wider(names_from = tipo_pregunta, values_from = text)
 
+  multitabla <- diccionario %>% filter(tipo_pregunta == "multitabla")%>%
+    select(tipo_pregunta, text, pregunta, bloque, llaves) #%>%
+  # pivot_wider(names_from = tipo_pregunta, values_from = text)
+
+  multitabla2 <- diccionario %>% filter(tipo_pregunta == "multitabla")%>%
+    select(tipo_pregunta, text, pregunta, bloque) %>%
+    pivot_wider(names_from = tipo_pregunta, values_from = text) %>% rename(respuestas_multitabla = multitabla)
 
   if(nrow(aspectos)>0){
     for(i in tipo_r){
@@ -61,19 +68,39 @@ diccionario_cuestionario <- function(doc, patron){
       rowwise() %>%
       mutate(tipo_pregunta = tr[which(!is.na(c_across(cols = all_of(tr))))]) %>%
       # unnest(tipo_pregunta) %>%
-      mutate(tipo_pregunta = if_else(is.na(tipo_pregunta), "multirespuesta", tipo_pregunta)) %>%
+      # mutate(tipo_pregunta = if_else(is.na(tipo_pregunta), "multirespuesta", tipo_pregunta)) %>%
       unite(text, tr) %>%
       mutate(text = stringr::str_replace_all(pattern = "_NA|NA_",replacement = "",string = text)) %>%
       left_join(diccionario %>% select(aspectos = text, pregunta, bloque, llaves)) %>%
       rename(tema = aspectos) %>% distinct(.keep_all = T)
 
+    if(nrow(multitabla)>0){
+      multitabla_aux <- aspectos %>% filter(tipo_pregunta == "multitabla") %>%
+        group_by(pregunta, bloque, tema, tipo_pregunta) %>%
+        summarise(respuestas_multitabla = list(text)) %>%
+        ungroup
+
+      aspectos <- aspectos %>% left_join(multitabla %>% rename(llaves_multitabla = llaves))
+      orden_multitabla <- diccionario %>% distinct(llaves) %>% left_join(aspectos %>% select(contains("llaves"))) %>%
+        mutate(llaves = if_else(!is.na(llaves_multitabla),
+                                paste(llaves, llaves_multitabla, sep = "_"), as.character(llaves)),
+               llaves = factor(llaves, unique(llaves))) %>% pull(llaves) %>% levels()
+      aspectos <- aspectos %>%
+        mutate(llaves = if_else(!is.na(llaves_multitabla),
+                                paste(llaves, llaves_multitabla, sep = "_"), as.character(llaves)))# %>%
+        # select(-llaves_multitabla)
+
+    }
+
   }
+
 
 
   diccionario <- diccionario %>%
     anti_join(diccionario %>% filter(tipo_pregunta == "aspectos"), by = "pregunta") %>%
     select(bloque, pregunta, tipo_pregunta, llaves, text) %>%
-    bind_rows(aspectos) %>% {if("tema" %in% names(.)) {
+    bind_rows(aspectos) %>%
+    {if("tema" %in% names(.)) {
       group_by(.,bloque, pregunta, tipo_pregunta, llaves, tema)
     } else {
       group_by(.,bloque, pregunta, tipo_pregunta, llaves)
@@ -84,6 +111,15 @@ diccionario_cuestionario <- function(doc, patron){
       .
     }} %>%
     summarise(respuestas=list(text)) %>%
+    {if(nrow(multitabla_aux)>0){
+      select(mutate(left_join(.,  multitabla2),
+                    nulo = map_lgl(respuestas_multitabla,is.null),
+                    respuestas = if_else(!nulo, respuestas_multitabla, respuestas),
+                    llaves = factor(llaves, orden_multitabla)
+      ), -respuestas_multitabla, -nulo)
+    } else{
+      .
+    }} %>%
     ungroup() %>% arrange(llaves)
 
   return(diccionario)
