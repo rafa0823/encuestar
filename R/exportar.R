7bce44ea4f085c3d9d1398cdde44f58ddbd29186
Allan <al.josue93@gmail.com>
2023-09-11 10:44:47 -0600

    -Arreglo de la función "exportar_bd": Se adaptó al nuevo formato del cuestionario (de word a excel). Comentar ordenar código.

--- R/exportar.R
+++ R/exportar.R
@@ -14,30 +14,28 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c("text", "Date", "Latitud
 
 exportar_bd <- function(self, carpeta, agregar, quitar){
 
-  vars <- self$cuestionario$documento %>%
-    filter(style_name == "Morant_filtros" | style_name == "Preguntas_filtros", str_detect(text,"\\{")) %>%
-    transmute(text = stringr::str_extract(text,"(?<=\\{).+?(?=\\})") %>% stringr::str_squish()) %>% pull(1)
+  # Variables provenientes del cuestionario
+  vars <- cuestionario |>
+    pull(llaves)
 
-  compartir <-
-    self$respuestas$base %>%
-    select(Fecha = Date, Latitud = Latitude,
-           Longitud = Longitude,
+  # Base ordenada con fecha, ubicación, variables del cuestionario, variables adicionales opcionales
+  # y sin variables eliminadas
+  compartir <- self$respuestas$base %>%
+    select(Fecha = Date, Latitud = Latitude, Longitud = Longitude,
            all_of(vars),
-           all_of(levels(droplevels(self$cuestionario$diccionario$llaves))),
            all_of(agregar)) %>%
     select(-any_of(quitar))
 
-  eliminar <-
-    names(self$respuestas$base)[is.na(match(names(self$respuestas$base),
-                                            names(compartir)))]
+  eliminar <- names(self$respuestas$base)[is.na(match(names(self$respuestas$base), names(compartir)))]
 
   continuar <- yesno::yesno2(glue::glue("Desea eliminar las siguientes variables?: \n {paste(eliminar, collapse = ', ')}"),
                              yes = "Si", no = "No")
 
   if(continuar){
+
     compartir %>% readr::write_excel_csv(glue::glue("{carpeta}/bd.csv"))
 
-  } else{
+  } else {
     cat("Use el parametro 'agregar' y haga un vector con las variables que desea agregar. \n O arregle las llaves del cuestionario")
   }
 }
