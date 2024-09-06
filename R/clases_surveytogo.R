decf2bed7c10f5332143e23535c862a18406d290
AllanJ93 <al.josue93@gmail.com>
2024-09-06 13:01:40 -0600

    Comprobar unicidad de llaves como candado

--- R/clases_surveytogo.R
+++ R/clases_surveytogo.R
@@ -812,23 +812,27 @@ Muestra <- R6::R6Class("Muestra",
 #'@export
 #'
 Cuestionario <- R6::R6Class("Cuestionario",
-                            public=list(
-                              documento=NULL,
-                              aprobado=NULL,
-                              diccionario=NULL,
-                              initialize=function(documento, patron){
+                            public = list(
+                              documento = NULL,
+                              aprobado = NULL,
+                              diccionario = NULL,
+                              initialize = function(documento, patron){
                                 if("data.frame" %in% class(documento)){
+                                  llaves_repetidas = encuestar:::comprobar_unicidadLlaves(diccionario = documento)
+                                  if(0 < llaves_repetidas) {
+                                    stop(glue::glue("Hay ", llaves_repetidas, " llaves repetidas en el diccionario"))
+                                  }
                                   self$diccionario <- documento
                                 } else{
                                   self$documento <- documento %>% officer::docx_summary() %>% as_tibble
                                   self$diccionario <- private$crear_diccionario(patron)
                                 }
                               },
-                              aprobar=function(){
+                              aprobar = function(){
                                 self$aprobado <- T
                                 return(invisible(self))
                               },
-                              checar_pregunta=function(pregunta){
+                              checar_pregunta = function(pregunta){
                                 pregunta_chr <- rlang::expr_text(ensym(pregunta))
                                 bd <- self$diccionario %>%
                                   filter(llaves==pregunta_chr)
@@ -847,11 +851,10 @@ Cuestionario <- R6::R6Class("Cuestionario",
                               }
                             )
                             ,
-                            private=list(
-                              crear_diccionario=function(patron){
-                                diccionario <- diccionario_cuestionario(self$documento, patron)
+                            private = list(
+                              crear_diccionario = function(patron){
+                                diccionario <- encuestar:::diccionario_cuestionario(self$documento, patron)
                                 return(diccionario)
-
                               })
 )
 
