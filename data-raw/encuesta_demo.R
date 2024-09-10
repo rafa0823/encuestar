1508c5f6b9a19450b3e4f622400a360f8bd66284
AllanJ93 <al.josue93@gmail.com>
2024-09-10 10:02:53 -0600

    Consolidar documentacion

--- data-raw/encuesta_demo.R
+++ data-raw/encuesta_demo.R
@@ -203,4 +203,5 @@ encuesta_demo <- Encuesta$new(respuestas = bd_respuestas_hermosillo_agosto,
                               auditar = c("")
 )
 
-usethis::use_data(encuesta_demo, encuesta_demo, internal = TRUE, overwrite = TRUE)
+# usethis::use_data(encuesta_demo, encuesta_demo, internal = TRUE, overwrite = TRUE)
+usethis::use_data(encuesta_demo)
