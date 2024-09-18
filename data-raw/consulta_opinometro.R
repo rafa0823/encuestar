3e28743f77a3290a7a0682000eab986cac1ae774
AllanJ93 <al.josue93@gmail.com>
2024-09-18 16:37:53 -0600

    Cambios a la consulta

--- data-raw/consulta_opinometro.R
+++ data-raw/consulta_opinometro.R
@@ -62,3 +62,29 @@ hola |>
   glimpse()
   select(ubicacion_aplicada, Latitude, Longitude)
   glimpse()
+
+
+  DBI::dbListTables(conn = pool)
+
+
+ds2 <-
+  dplyr::tbl(src = pool, "ds_e2") |>
+    dplyr::collect()
+
+
+tibble("Dato" = c("Registros", "Duracion (minutos")) |>
+  bind_cols(ds2 |>
+              filter(lubridate::as_date("2023-07-01") <= fecha_inicio, fecha_inicio <= lubridate::as_date("2023-08-01")) |>
+              count(fecha = lubridate::as_date(fecha_inicio)) |>
+              tidyr::pivot_wider(names_from = fecha, values_from = n) |>
+              bind_rows(ds2 |>
+                          filter(lubridate::as_date("2023-07-01") <= fecha_inicio, fecha_inicio <= lubridate::as_date("2023-08-01")) |>
+                          transmute(fecha = lubridate::as_date(fecha_inicio),
+                                    duracion = difftime(fecha_fin, fecha_inicio, units = "mins")) |>
+                          group_by(fecha) |>
+                          summarise(duracion = sum(duracion, na.rm = TRUE)/lubridate::dhours(1)) |>
+                          tidyr::pivot_wider(names_from = fecha, values_from = duracion))) |>
+  writexl::write_xlsx(path = "data-raw/dialogo_social_e2_registros_duracion.xlsx")
+
+
+
