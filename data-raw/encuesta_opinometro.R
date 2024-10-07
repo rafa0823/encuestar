799127a47b511fcd5accbaf1e79b0cf90d043fe9
AllanJ93 <al.josue93@gmail.com>
2024-10-07 09:37:19 -0600

    Actualizar script encuesta opinometro

--- data-raw/encuesta_opinometro.R
+++ data-raw/encuesta_opinometro.R
@@ -6,7 +6,8 @@
 # Preambulo -----------------------------------------------------------------------------------
 
 library(dplyr)
-devtools::load_all()
+library(encuestar)
+# devtools::load_all()
 
 # Insumos -------------------------------------------------------------------------------------
 
@@ -19,109 +20,6 @@ diseno_hermosillo_agosto <-
 diccionario_sonora_agosto <-
   readxl::read_xlsx(path = "./data-raw/diccionario_sonora_20240802.xlsx") |>
   filter(!llaves %in% c("gps", "intentos"))
-
-# data-raw ------------------------------------------------------------------------------------
-
-# pool <-
-#   pool::dbPool(
-#     drv = odbc::odbc(),
-#     Driver= 'ODBC Driver 17 for SQL Server',
-#     Database = "SVNET",
-#     Server = "tcp:morant.database.windows.net",
-#     UID = "emorones",
-#     PWD = "Mor@nt2024",
-#     Port = 1433,
-#     timeout = 120
-#   )
-#
-# obtener_respuesta <- function(pool, codigos, encuesta_id){
-#   query_claves <- paste0("REPLACE(JSON_VALUE(r.Resultado, '$.", codigos, "'), 'ñ', 'n') AS ", codigos, collapse = ", ")
-#   encuesta_id <- encuesta_id |> toString()
-#
-#   query <- glue::glue("
-#   SELECT
-#     r.Id,
-#     r.EncuestaId,
-#     r.FechaInicio,
-#     r.FechaFin,
-#     r.FechaCreada,
-#     r.UbicacionAplicada,
-#     r.UsuarioNum,
-#     r.isComplete,
-#     ", query_claves, "
-#   FROM
-#     Registros r
-#   WHERE EncuestaId in ({encuesta_id})
-# ")
-#
-#   dplyr::tbl(pool, dplyr::sql(query)) |>
-#     janitor::clean_names()
-# }
-
-## Metodo con base en variables ---------------------------------------------------------------
-
-# variables_id163 <-
-#   tbl(src = pool, "Registros") |>
-#   filter(EncuestaId == 163) |>
-#   collect() %>%
-#   purrr::pmap_df(function(Id, fecha, Resultado, UsuarioNum, ...){
-#     aux <- Resultado |>
-#       jsonlite::fromJSON() |>
-#       as_tibble()}) |>
-#   select(!c(starts_with("Pregunta"), ObtenerGPS)) |> # Omitir variables que no tienen nombre o desconocidas
-#   colnames()
-#
-# bd_raw_id163 <-
-#   obtener_respuesta(pool = pool,
-#                     codigos = variables_id163,
-#                     encuesta_id = 163) |>
-#   left_join(tbl(pool, "Usuarios") |>
-#               semi_join(
-#                 tbl(pool, "UsuariosEncuesta") |>
-#                   filter(EncuestaId == 163), join_by(Id == UsuarioId)) |>
-#               select(Num, Nombre, APaterno, AMaterno),
-#             by = c("usuario_num" = "Num")) |>
-#   dplyr::collect() |>
-#   filter(ubicacion_aplicada != "No aplica") |>
-#   mutate(ubicacion_aplicada = dplyr::if_else(condition = ubicacion_aplicada == ",",
-#                                              true = NA_character_,
-#                                              false = ubicacion_aplicada)) |>
-#   tidyr::separate(col = ubicacion_aplicada,
-#                   into = c("Latitude", "Longitude"),
-#                   sep = ",",
-#                   remove = FALSE) %>%
-#   transmute(
-#     SbjNum = id,
-#     Date = lubridate::as_datetime(fecha_inicio),
-#     Latitude, Longitude,
-#     VStart = lubridate::as_datetime(fecha_inicio),
-#     VEnd = lubridate::as_datetime(fecha_fin),
-#     Srvyr = paste0(Nombre, APaterno, AMaterno),
-#     across(all_of(variables_id163))
-#   )
-#
-# bd_id163 <-
-#   bd_raw_id163 %>%
-#   mutate(SbjNum = row_number(),
-#          edad = sample(seq.int(from = 18, to = 80, by = 1),
-#                        size = nrow(.),
-#                        replace = TRUE),
-#          sexo = sample(c("Mujer", "Hombre"),
-#                        size = nrow(.),
-#                        replace = TRUE),
-#          SECCION = sample(diseno_hermosillo_agosto$muestra$SECCION |>
-#                             distinct(cluster_2) |>
-#                             mutate(cluster_2 = as.character(cluster_2)) |>
-#                             pull(),
-#                           size = nrow(.),
-#                           replace = TRUE),
-#          problema_principal = sample(c("A", "B", "C"),
-#                                      size = nrow(.),
-#                                      replace = TRUE),
-#          MUNI = sample(c("mun_a", "mun_a", "mun_a"),
-#                                      size = nrow(.),
-#                                      replace = TRUE)
-#          )
 
 # Base de eliminadas --------------------------------------------------------------------------
 
