abd5be0799d7a43537c174cc363ad93fdeecdda0
AllanJ93 <al.josue93@gmail.com>
2024-10-16 12:03:37 -0600

    Documentacion de la clase encuesta

--- R/clases_surveytogo.R
+++ R/clases_surveytogo.R
@@ -3,49 +3,43 @@
 #' @description La clase encuesta es la interfaz de usuario de la paquetería encuestar.
 #'  A través de la clase encuesta se producen los resultados y se puede consultar información
 #'  relacionada al levantamiento, respuestas y equipo que trabaja en campo. La clase Encuesta tiene
-#'  cuatro clases subordinadas: `Respuestas`, `Muestra`, `Resultados` y `Auditoria`.
+#'  cinco clases subordinadas: `Cuestionario`, `Respuestas`, `Muestra`, `Resultados` y `Auditoria`.
 #'
-#' @field muestra Objeto tipo `.rda` generado por la paquetería `muestrear`. Es el diseño muestral
-#'  de la encuesta. El dobjeto `muestra` es de formato personalizado por lo que no es posible
-#'  (o práctico) sustituirlo.
-#' @field shp Objeto tipo `.rda` generado por la paquetería `muestrear`. Contiene toda la cartografía
-#'  utilizada para el equipo de campo y que a su vez es utilizada por la aplicación de monitoreo del
-#'  levantamiento. El objeto `shp` es de formato personalizado por lo que no es posible (o práctico)
-#'  sustituirlo.
-#' @field cuestionaro [tibble()] que es el diccionario del cuestionario que se aplica a los entrevistados.
-#'  El parámetro `cuestionario`, después de una actualización, actúa como el diccionario (o codebbok).
-#' @field respuestas [tibble()] de respuestas obtenidas por las personas entrevistadas.
-#' @field n_simulaciones Valor entero. Con el objetivo de publicar la aplicación de monitoreio antes
-#'  de inciar el levantamiento es posible simular una base de respuestas. `n_simulaciones` es el
-#'  total de respuestas simuladas, es decir, es el [nrow()] que simula al campo `respuestas`.
-#'  El valor recomendado es del 10% del total de entrevistas objetivo de la encuesta.
-#' @field opinometro_id Valor entero. Identificador del cuestionario aplicado en campo generado en
-#'  la plataforma `Opinómetro`. Es necesario consultar a la persona que construyó el cuestionario
-#'  para conocer el `opinometro_id` asociado.
-#' @field pool Objeto tipo [pool] generado al conectarse a la base de datos que almacena las
-#'  respuestas. Se recomienda utilizar la función [dbPool()] de la paquetería [pool] para esto.
-#' @field bd_categorias [tibble()] que contiene los resultados generados por IA a partir de las
-#'  preguntas abiertas.
-#' @field bd_correcciones [tibble()] que contiene las correcciones de los registros de las encuestas
-#'  efectivas.
-#' @field patron Valor tipo caracter que indica qué cadenas de texto quitar de las posibles opciones
-#'  de respuesta a las preguntas para no presentarlas en los resultados.
-#' @field auditoria_telefonica [tibble()] que contiene las entrevistas que, por auditoría telefónica,
-#'  han sido eliminadas de los registros.
-#' @field quitar_vars `DEPRECATED` Vector tipo caracter que contiene las variables que se desean
-#'  omitir del procesamiento.
-#' @field mantener Vector tipo caracter que indica los clusters a los cuales habrá que forzar las
-#'  entrevistas que se hayan levantado cerca de los mismos.
-#' @field auditar Vector tipo caracter que contiene las varaibles que se mostraán en la aplicación
-#'  de monitoreo de levantamiento en la pestala `Resultados`.
-#' @field vars_tendencias Vector tipo caracter que contiene las variables que se mostrarán en la
-#'  aplicación de monitoreo de levantamiento en la sección `Tendencias`.
-#' @field sin_peso `LOGICAL` Determina si esl diseno muestral construido será ponderado o no.
-#' @field rake `LOGICAL` Determina si el diseno muestral será postestratificado por edad y sexo.
-#' @field mantener_falta_coordenadas `LOGICAL`. Determina si se descartan o no las entrevistas sin
-#'  geolocalización válida o nula.
-#' @field tipo_encuesta `DEPRECATED`. La paquetería cuenta con un modo de cálculo de diseño muestral
-#'  análogo al `INEGI`. Sin embargo, este modo ha entrado en desuso y en un futuro desaparecerá.
+#' @field muestra El campo `muestra` contiene los resultados de ejecutar la clase `Muestra` de
+#'  jerarquía menor. Dicha clase contiene el diseño muestral calculado a partir de la edad, sexo y
+#'  geolocalización de los individuos entrevistados. Además del diseño muestral contiene métodos para
+#'  modificar dicho diseño a partir de postestratificaciones o filtrar a través de subconjuntos de
+#'  la muestra.
+#' @field shp El campo `shp` contiene la cartografía de los clusters que hayan sido seleccionados en
+#'  el proceso de muestreo.
+#' @field cuestionario El campo `cuestionario` contiene los resultados de ejecutar la clase
+#'  `Cuestionario`. Dicha clase originalmente recibía el texto y las clases asociadas al cuestinoario
+#'  en formato .docx y generaba el diccionario. Actualmente la clase recibe y asigna el [tibble()]
+#'  que contiene el diccionario y realiza unas cuantas verificaciones.
+#' @field respuestas El campo `respuestas` contiene los resultados de ejecutar la clase `Respuestas`
+#'  de jerarquía menor. Dicha clase tiene como objetivo verificar, limpiar y estandarizar las
+#'  respuestas recibidas de campo.
+#' @param n_simulaciones Valor entero usado para simular un [tibble()] necesario para generar
+#'  la clase `Respuestas`.
+#' @field opinometro_id Valor entero usado para generar el [tibble()] de respuestas a través
+#'  de la plataforma Opinómetro y que es necesario para generar la clase `Respuestas`.
+#' @field pool El campo `pool` se hereda para ser usado por la clase `Opinómetro`.
+#' @field bd_categorias El [tibble()] que contiene las categoriías generadas por IA se une al
+#'  [tibble()] `respuestas` independientemente si el segundo es del campo `respuestas` o
+#'  generado por opinómetro.
+#' @field bd_correcciones El campo se hereda a la clase `Respuestas`.
+#' @field patron El campo se hereda a la clase `Respuestas`.
+#' @field auditoria_telefonica El campo se hereda a la clase `Respuestas`.
+#' @field quitar_vars `DEPRECATED` en futuro desuso.
+#' @field mantener El campo se hereda a la clase `Respuestas`.
+#' @field auditar El campo se queda guardado en la clase `Encuesta` para su uso en la aplicación
+#'  de monitoreo del levantamiento.
+#' @field vars_tendencias El campo se queda guardado en la clase `Encuesta` para su uso en la
+#'  aplicación de monitoreo del levantamiento.
+#' @field sin_peso El campo se hereda a la clase `Respuestas`.
+#' @field rake El campo se hereda a la clase `Respuestas`.
+#' @field mantener_falta_coordenadas El campo se hereda a la clase `Respuestas`.
+#' @field tipo_encuesta Por defecto se imputa que el tipo de encuesta sea `ine`.
 #' @field shp_completo Extensión del campo `shp`. Una vez calculadas las entrevistas efectivas y
 #'  asignadas las ponderaciones. Se determinan las ubicaciones puntuales donde se levantaron las
 #'  entrevistas y se agrega al objeto `shp`.
@@ -56,7 +50,6 @@
 #'
 #' @export
 #' @import dplyr ggplot2 tidyr sf purrr stringr
-
 Encuesta <-
   R6::R6Class(
     classname = "Encuesta",
@@ -76,36 +69,79 @@ Encuesta <-
       mantener = NULL,
       auditar = NA,
       vars_tendencias = NULL,
-      sin_peso = NA,
+      sin_peso = NULL,
       rake = NA,
       mantener_falta_coordenadas = NULL,
       tipo_encuesta = NULL,
       shp_completo = NULL,
       Resultados = NULL,
       Auditoria = NULL,
-      #' @description
-      #' Create a person
-      #' @param respuestas Name of the person
-      #' @param diccionario Hair colour
-      initialize = function(respuestas = NA,
-                            n_simulaciones = NULL,
-                            opinometro_id = NULL,
-                            pool = NULL,
-                            bd_categorias = NULL,
-                            quitar_vars = c(),
-                            muestra = NA,
-                            auditoria_telefonica = NA,
-                            bd_correcciones = NULL,
-                            cuestionario = NA,
+      #' @description Se reciben los insumos de respuestas, auditoria y otros parámetros asociados
+      #'  al levantamiento de la encuesta para construir el diseño muestral y las clases posteriores
+      #'  para generar resultados.
+      #' @param muestra Objeto tipo `.rda` generado por la paquetería `muestrear`. Es el diseño muestral
+      #'  de la encuesta. El dobjeto `muestra` es de formato personalizado por lo que no es posible
+      #'  (o práctico) sustituirlo.
+      #' @param shp Objeto tipo `.rda` generado por la paquetería `muestrear`. Contiene toda la
+      #'  cartografía utilizada para el equipo de campo y que a su vez es utilizada por la aplicación
+      #'  de monitoreo del levantamiento. El objeto `shp` es de formato personalizado por lo que no
+      #'  es posible (o práctico) sustituirlo.
+      #' @param cuestionario [tibble()] que es el diccionario del cuestionario que se aplica a los
+      #'  entrevistados. El parámetro `cuestionario`, después de una actualización, actúa como el
+      #'  diccionario (o codebbok).
+      #' @param respuestas [tibble()] de respuestas obtenidas por las personas entrevistadas.
+      #' @param n_simulaciones Valor entero. Con el objetivo de publicar la aplicación de monitoreio
+      #'  antes de inciar el levantamiento es posible simular una base de respuestas.
+      #'  `n_simulaciones` es el total de respuestas simuladas, es decir, es el [nrow()] que simula
+      #'  al campo `respuestas`. El valor recomendado es del 10% del total de entrevistas objetivo
+      #'  de la encuesta. Es mutuamente excluyente con el campo `respuestas`.
+      #' @param opinometro_id Valor entero. Identificador del cuestionario aplicado en campo generado
+      #'  en la plataforma `Opinómetro`. Es necesario consultar a la persona que construyó el
+      #'  cuestionario para conocer el `opinometro_id` asociado. Es mutuamente excluyente con el
+      #'  campo `respuestas`.
+      #' @param pool Objeto tipo [pool] generado al conectarse a la base de datos que almacena las
+      #'  respuestas. Se recomienda utilizar la función [dbPool()] de la paquetería [pool] para esto.
+      #' @param bd_categorias [tibble()] que contiene los resultados generados por IA a partir de las
+      #'  preguntas abiertas.
+      #' @param bd_correcciones [tibble()] que contiene las correcciones de los registros de las encuestas
+      #'  efectivas.
+      #' @param patron Valor tipo caracter que indica qué cadenas de texto quitar de las posibles opciones
+      #'  de respuesta a las preguntas para no presentarlas en los resultados.
+      #' @param auditoria_telefonica [tibble()] que contiene las entrevistas que, por auditoría telefónica,
+      #'  han sido eliminadas de los registros.
+      #' @param quitar_vars `DEPRECATED` Vector tipo caracter que contiene las variables que se desean
+      #'  omitir del procesamiento.
+      #' @param mantener Vector tipo caracter que indica los clusters a los cuales habrá que forzar las
+      #'  entrevistas que se hayan levantado cerca de los mismos.
+      #' @param auditar Vector tipo caracter que contiene las varaibles que se mostraán en la aplicación
+      #'  de monitoreo de levantamiento en la pestala `Resultados`.
+      #' @param vars_tendencias Vector tipo caracter que contiene las variables que se mostrarán en la
+      #'  aplicación de monitoreo de levantamiento en la sección `Tendencias`.
+      #' @param sin_peso `LOGICAL` Determina si esl diseno muestral construido será ponderado o no.
+      #' @param rake `LOGICAL` Determina si el diseno muestral será postestratificado por edad y sexo.
+      #' @param mantener_falta_coordenadas `LOGICAL`. Determina si se descartan o no las entrevistas sin
+      #'  geolocalización válida o nula.
+      #' @param tipo_encuesta `DEPRECATED`. La paquetería cuenta con un modo de cálculo de diseño muestral
+      #'  análogo al `INEGI`. Sin embargo, este modo ha entrado en desuso y en un futuro desaparecerá.
+      initialize = function(muestra = NA,
                             shp = NA,
-                            tipo_encuesta = "ine",
-                            mantener = "",
+                            cuestionario = NA,
+                            respuestas = NA,
+                            n_simulaciones = NA,
+                            opinometro_id = NA,
+                            pool = NA,
+                            bd_categorias = NA,
+                            bd_correcciones = NA,
                             patron = NA,
+                            auditoria_telefonica = NA,
+                            quitar_vars = c(),
+                            mantener = "",
                             auditar = NA,
-                            vars_tendencias = NULL,
-                            sin_peso = F,
-                            rake = T,
-                            mantener_falta_coordenadas = FALSE) {
+                            vars_tendencias = NA,
+                            sin_peso = FALSE,
+                            rake = TRUE,
+                            mantener_falta_coordenadas = FALSE,
+                            tipo_encuesta = "ine") {
         sf_use_s2(F)
         tipo_encuesta <- match.arg(tipo_encuesta, c("inegi","ine"))
         self$sin_peso <- sin_peso
